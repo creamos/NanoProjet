fe34f8069e9910e35bd9772e2d4e3432194418ac
Geegaz <geegazrd@gmail.com>
2022-12-09 15:50:38 +0100

    working obstacles generation

---
+++ Assets/ColdResources/Scripts/Utilities/GenerationUtil.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class GenerationUtil : MonoBehaviour
+{
+    [SerializeField] private MovingObjectsManager generator;
+    
+    [Button]
+    private void StartGenerator() {
+        if (generator != null) generator.StartGenerating();
+    }
+    [Button]
+    private void StopGenerator() {
+        if (generator != null) generator.StopGenerating();
+    }
+}
