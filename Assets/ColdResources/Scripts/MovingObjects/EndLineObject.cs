0cdfcbd8f16d01984199e899cbc3f52e1d2f07a4
Geegaz <geegazrd@gmail.com>
2022-12-12 14:14:53 +0100

    added quick EndLine

---
+++ Assets/ColdResources/Scripts/MovingObjects/EndLineObject.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class EndLineObject : MonoBehaviour
+{
+    // Very ugly code for playtests
+    [SerializeField] private PlayersManagerSO _playersManager;
+    [SerializeField] private float _endWaitTime = 3.0f;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+        if (playerMovement) {
+            StartCoroutine("EndProcess");
+            foreach (PlayerInput player in _playersManager._players)
+            {
+                playerMovement = player.GetComponent<PlayerMovement>();
+                playerMovement.enabled = false;
+            }
+        }
+    }
+
+    IEnumerator EndProcess() {
+        yield return new WaitForSeconds(_endWaitTime);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
