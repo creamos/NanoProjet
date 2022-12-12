2a007ffaa2c0b1561bec17ecd800dc83bf108a35
Geegaz <geegazrd@gmail.com>
2022-12-12 10:18:34 +0100

    Added obstacles to test scene

--- Assets/ColdResources/Scripts/ScriptableObjects/MovementDataSO.cs
+++ Assets/ColdResources/Scripts/ScriptableObjects/MovementDataSO.cs
@@ -9,16 +9,14 @@ public class MovementDataSO : ScriptableObject
     [Range(0.0f, 100.0f)] public float fallingSpeed = 10.0f; // m/s
     [Range(0.0f, 100.0f)] public float movementSpeed = 10.0f; // m/s
 
-    [Space]
+    [Header("Knockback")]
     [Range(0.0f, 1.0f)] public float knockbackMultiplier = 0.8f;
-    [Range(1.0f, 2.0f)] public float boostMultiplier = 1.5f;
-
-    [Space]
     public float knockbackTime = 0.5f;
-    public float boostTime = 0.5f;
-
-    [Space]
     [CurveRange(0.0f, 0.0f, 1.0f, 1.0f, EColor.Red)] public AnimationCurve knockbackProfile;
+
+    [Header("Boost")]
+    [Range(1.0f, 2.0f)] public float boostMultiplier = 1.5f;
+    public float boostTime = 0.5f;
     [CurveRange(0.0f, 0.0f, 1.0f, 1.0f, EColor.Blue)] public AnimationCurve boostProfile;
 
 
