dcf8a1b8c6ac78f97d555a9d11c11655b117563a
Geegaz <geegazrd@gmail.com>
2022-12-13 19:12:25 +0100

    Added end line phase

--- Assets/ColdResources/Scripts/MovingObjects/EndLineObject.cs
+++ Assets/ColdResources/Scripts/MovingObjects/EndLineObject.cs
@@ -6,23 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class EndLineObject : MonoBehaviour
 {
-    // Very ugly code for playtests
-    [SerializeField] private PlayersManagerSO _playersManager;
+    // Very ugly code - and that's it
     [SerializeField] private float _endWaitTime = 3.0f;
 
     private void OnTriggerEnter2D(Collider2D other) {
         PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
         if (playerMovement) {
             StartCoroutine("EndProcess");
-            foreach (PlayerInput player in _playersManager.players)
-            {
-                playerMovement = player.GetComponent<PlayerMovement>();
-                playerMovement.enabled = false;
-            }
         }
     }
 
     IEnumerator EndProcess() {
+        GameObject target_vcam = GameObject.FindGameObjectWithTag("GameplayCamera");
+        target_vcam.SetActive(false);
+        
         yield return new WaitForSeconds(_endWaitTime);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
