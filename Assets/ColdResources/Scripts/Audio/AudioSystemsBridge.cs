f1b595dd3a8db79d65c82f081db4885fea9a30f1
Alex <alexandrelacroix.35@gmail.com>
2022-12-13 23:50:11 +0100

    Work on audio assets

--- Assets/ColdResources/Scripts/Audio/AudioSystemsBridge.cs
+++ Assets/ColdResources/Scripts/Audio/AudioSystemsBridge.cs
@@ -17,7 +17,7 @@ public class AudioSystemsBridge : MonoBehaviour
     {
         _managerSO.RunStarted.AddListener(_manager.StartMusic);
         _managerSO.PlayerObstacleCollision.AddListener(_manager.PlaySlowSound);
-        _managerSO.PlayerBoost.AddListener(_manager.PlayBoostSound);
+        //_managerSO.PlayerBoost.AddListener(_manager.PlayBoostSound);
         _managerSO.PaceChanged.AddListener(_manager.ChangeMusicToHardmode);
     }
 
@@ -25,7 +25,7 @@ public class AudioSystemsBridge : MonoBehaviour
     {
         _managerSO.RunStarted.RemoveListener(_manager.StartMusic);
         _managerSO.PlayerObstacleCollision.RemoveListener(_manager.PlaySlowSound);
-        _managerSO.PlayerBoost.RemoveListener(_manager.PlayBoostSound);
+        //_managerSO.PlayerBoost.RemoveListener(_manager.PlayBoostSound);
         _managerSO.PaceChanged.RemoveListener(_manager.ChangeMusicToHardmode);
     }
 }
